5a3cb2080a186503836b9f0adc93f85cda532f91
Anirudh Sarve <107532050+anisarve2006@users.noreply.github.com>
2024-11-02 00:25:52 +0530

    app js file updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const path = require('path');
 
 app.set('view engine' , "ejs");
@@ -8,5 +9,5 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/' , (req, res) => {
-    res.render('index')
+    res.render('index');
 });
