089116a90a5bb41c7ff38e85bed3a3d98edae442
Anirudh Sarve <107532050+anisarve2006@users.noreply.github.com>
2024-11-02 18:04:04 +0530

    Components folder added

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/' , (req, res) => {
     res.send('Dev');
 });
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
