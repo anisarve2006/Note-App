a6ade191eb3d74f53459910ae9ec2aca6578e042
Anirudh Sarve <107532050+anisarve2006@users.noreply.github.com>
2024-11-03 19:59:26 +0530

    Update app.js
    app.listen redandancy rsolved

--- app.js
+++ app.js
@@ -44,11 +44,6 @@ app.post("/upload", upload.single("image"), (req, res) => {
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port http://localhost:3000/`);
-  });
+});
   
-=======
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
 
