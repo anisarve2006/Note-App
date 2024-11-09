62158edc925d35223655261b8ce45d651251b11d
Anirudh Sarve <107532050+anisarve2006@users.noreply.github.com>
2024-11-10 02:24:41 +0530

    Redirected Updation

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();  // to read .env file (environment variable)
 const express = require('express');
 const path = require('path');
-//const noteRouter = require('./routers/noteRouter.js');
-const userRouter = require('./routers/userRouter.js');
+//const noteRouter = require('./routers/noteRouter');
+const userRouter = require('./routers/userRouter');
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -15,11 +15,33 @@ app.use(express.static(path.join(__dirname,'public')));
 //Routing 
 app.use('/api/user/', userRouter);
 //app.use('/api/note/', noteRouter);
+app.use('/api/verification', verificationRouter)
+
+
+
+// In-memory Storage for verification code
+const verificationCodes = new Map();
+function storeVerificationCode(email, code) {
+  verificationCodes.set(email, code);
+  // Auto-delete code after 10 minutes
+  setTimeout(() => verificationCodes.delete(email), 10 * 60 * 1000);
+}
+function getVerificationCode(email) {
+  return verificationCodes.get(email);
+}
+module.exports = {
+  storeVerificationCode,
+  getVerificationCode,
+};
+
+
 
 // Routing parameters
 app.get('/' , (req, res) => {
     res.render('registration');
 });
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port http://localhost:3000/`);
