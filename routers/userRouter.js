9a3b069d255f0b340f21ef5eeb6650f4f02bd92c
Anirudh Sarve <107532050+anisarve2006@users.noreply.github.com>
2024-11-27 11:36:29 +0530

    Formated

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,13 +4,12 @@ const multer = require('multer');
 const { create, login, update, logout, remove } = require('../components/user');
 const { storage } = require('../config/cloudconfig');
 const upload = multer({ storage });
-const isLoggedIn = require("../config/isLoggedIn");
-
 
 // POST routes
 // verification requirements 
 const {sendVerificationEmail, generateVerificationCode} = require('../components/mail');
 const {storeVerificationCode, getVerificationCode, deleteVerificationCode} = require('../config/verification');
+const isLoggedIn = require('../config/isLoggedIn');
 // Endpoint to send the verification code
 router.post('/send-verification', async (req, res) => {
     const email = req.body.email;
@@ -23,7 +22,6 @@ router.post('/send-verification', async (req, res) => {
 router.post('/verify-code', (req, res) => {
     const { email, code } = req.body;
     const storedCode = getVerificationCode(email);
-  
     if (storedCode && storedCode === parseInt(code, 10)) {
       deleteVerificationCode(email); // Clear code on successful verification
       console.log('User verified');
@@ -38,6 +36,7 @@ router.route("/create").get((req, res) => res.render("registration")).post(uploa
 router.route("/login").get((req, res) => res.render("login")).post(login);
 router.route("/forgotpassword").get((req, res) => res.render("forgotPassword"));
 router.route("/logout").post(logout);
+router.route("/remove").get(isLoggedIn, remove);
 
 module.exports = router;
 
